9d3c2972e527a039cd8065eb47efef936c4445f7
Akshat Trivedi <akshattrivedi32@gmail.com>
2026-05-09 20:37:31 +0530

    Round Robin Load Balancer

---
+++ server1/src/main/java/com/example/server1/Controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.server1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Response from Spring Boot Server 1";
+    }
+}
---
+++ server2/src/main/java/com/example/server2/Controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.server2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Response from Spring Boot Server 2";
+    }
+}
